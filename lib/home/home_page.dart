a8c42dea386f1e4284fe12fa644a699179c33977
Lina Melo <lina@w12.com.br>
2021-04-26 09:42:40 -0300

    Aula 02 - componentes da home

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,9 +1,9 @@
 import 'package:devquiz/home/widgets/appbar/app_bar_widgets.dart';
+import 'package:devquiz/home/widgets/level_button/level_button_widget.dart';
 import 'package:flutter/material.dart';
 
-
 class HomePage extends StatefulWidget {
-  HomePage({Key? key}):super(key: key);
+  HomePage({Key? key}) : super(key: key);
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -13,6 +13,29 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarWidget(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            LevelButtonWidget(
+              label: "Fácil",
+            ),
+            LevelButtonWidget(
+              label: "Médio",
+            ),
+            LevelButtonWidget(
+              label: "Difícil",
+            ),
+            LevelButtonWidget(
+              label: "Perito",
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
+
+// Parei em 00:35:43 - aula 02
