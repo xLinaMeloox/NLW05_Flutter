087ca687d4fa0f4f3b4c0be3d42052e7a2a5251a
Lina Melo <lina@w12.com.br>
2021-04-26 08:40:40 -0300

    Aula 1

---
+++ lib/core/app_widget.dart
@@ -0,0 +1,14 @@
+
+import 'package:devquiz/home/home_page.dart';
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner:false,
+      title: "DevQuiz",
+      home: HomePage(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:devquiz/core/app_widget.dart';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(AppWidget());
+}
+
+
