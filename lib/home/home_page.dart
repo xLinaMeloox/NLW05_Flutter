e8f5e528352055f52b74d5767c315bd055b70c99
Lina Melo <lina@w12.com.br>
2021-04-27 09:04:10 -0300

    Adicionando o componente de card na home

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:devquiz/home/widgets/appbar/app_bar_widgets.dart';
 import 'package:devquiz/home/widgets/level_button/level_button_widget.dart';
+import 'package:devquiz/home/widgets/quiz_card/quiz_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -15,27 +16,29 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBarWidget(),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Column(
           children: [
-            LevelButtonWidget(
-              label: "Fácil",
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                LevelButtonWidget(
+                  label: "Fácil",
+                ),
+                LevelButtonWidget(
+                  label: "Médio",
+                ),
+                LevelButtonWidget(
+                  label: "Difícil",
+                ),
+                LevelButtonWidget(
+                  label: "Perito",
+                ),
+              ],
             ),
-            LevelButtonWidget(
-              label: "Médio",
-            ),
-            LevelButtonWidget(
-              label: "Difícil",
-            ),
-            LevelButtonWidget(
-              label: "Perito",
-            ),
+            QuizCardWidget(),
           ],
         ),
       ),
     );
   }
 }
-
-
-// Parei em 00:35:43 - aula 02
---
+++ lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -0,0 +1,58 @@
+import 'package:devquiz/core/app_colors.dart';
+import 'package:devquiz/core/core.dart';
+import 'package:flutter/material.dart';
+
+class QuizCardWidget extends StatelessWidget {
+  const QuizCardWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        border: Border.fromBorderSide(BorderSide(color: AppColors.border)),
+        color: AppColors.white,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+        Container(
+          height: 40,
+          width: 40,
+          // color: AppColors.red,
+          child: Image.asset(AppImages.blocks),
+        ),
+        SizedBox(
+          height: 24,
+        ),
+        Text(
+          "Gerenciamento de Estado",
+          style: AppTextStyles.heading15,
+        ),
+         SizedBox(
+          height: 24,
+        ),
+        Row(children: [
+          Expanded(
+            flex: 1,
+            child: Text(
+              "3 de 10",
+              style: AppTextStyles.body11,
+            ),
+          ),
+          Expanded(
+            flex: 4,
+            child: LinearProgressIndicator(
+              value: 0.3,
+              backgroundColor: AppColors.chartSecondary,
+              valueColor: AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
+            ),
+          ),
+        ])
+      ]),
+    );
+  }
+}
+
+//Parei no 00:50:04 - aula 02
